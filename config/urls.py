b919c81277b9b725213a830d893767993998a631
Dorota Ulenberg <dorota.ulenberg@wp.pl>
2026-02-06 19:43:41 +0100

    fix_urls

--- config/urls.py
+++ config/urls.py
@@ -3,16 +3,15 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from patients_login.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home'),
+    path('', RedirectView.as_view(url='/get-doctor/home-page/', permanent=False), name='home'),
     path('patients_login/', include('patients_login.urls')),
     path('patients_registration/', include('patients_registration.urls')),
     path('get-doctor/', include('registrations_visit.urls')),
-    path('rezerwacje/', include('booking.urls')),
-    path('home-home', HomeView.as_view(), name='home'),
+    path('booking/', include('booking.urls')),
+    #path('home-home', HomeView.as_view(), name='home'),
 ]
 
 if settings.DEBUG:
--- patients_login/urls.py
+++ patients_login/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login/', views.PatientLoginView.as_view(), name='login'),
     path('logout/', views.PatientLogoutView.as_view(), name='logout'),
     path('profile/', views.PatientProfileView.as_view(), name='profile'),
-    path('appointments/', views.PatientAppointmentsView.as_view(), name='appointments'),
+    #path('appointments/', views.PatientAppointmentsView.as_view(), name='appointments'),
 ]
 
